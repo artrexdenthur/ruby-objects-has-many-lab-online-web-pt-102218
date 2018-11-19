1452f022d138a6f04ff06fd86640f4c42284fbef
Paul Ashour <paul.ashour@gmail.com>
2018-11-19 04:41:04 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,8 @@ class Author
   
   attr_accessor :name, :posts
   
+  @@all = []
+  
   def initialize(name)
     @name = name
     @posts = []
@@ -14,6 +16,12 @@ class Author
   
   def add_post_by_title(title)
     add_post(Post.new(title))
+  end
+  
+  def self.post_count
+    
+  def self.all
+    @@all
   end
   
 end
